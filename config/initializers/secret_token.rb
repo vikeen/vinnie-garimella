c4c956a874a43f0eda66cb7f1c63de2f00e92165
vikeen <john.rake12@gmail.com>
2013-07-09 15:51:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vgarimella::Application.config.secret_token = 'b90823e7bbcef8a01b3b00af089c280a5b554f8994b52b255b7e40cc27157a242fce1ea96b0e6289f72e8f5f39843141a76cd4692524c577bbbb5171c2b661f7'
